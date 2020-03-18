fd94e72264e34812d1a5f4a37897a6c601e365f3
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-18 17:08:45 +0530

    MODIFIED:Displayed employee is present or not

--- StartProblem/employeeWage.sh
+++ StartProblem/employeeWage.sh
@@ -1,3 +1,10 @@
-#!/bin/bash -x
-#DISPLAYED THE WELCOME STATEMENT
-echo "Welcome to employee wage computation"
+#!/bin/bash
+#printing
+echo "We are printing employee is present or not"
+isPresent=$(($RANDOM%2))
+if((isPresent == 1))
+then
+	echo "Employee is present"
+else
+	echo "Employee is not present"
+fi
