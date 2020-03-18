e6cb2d5f3854d2af25d3d9b4d5802b451ab0ca5e
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-18 18:50:02 +0530

    MODIFIED:Using for loop and finded total salary

--- StartProblem/employeeWage.sh
+++ StartProblem/employeeWage.sh
@@ -1,19 +1,25 @@
 #!/bin/bash -x
-#ADDING PART TIME EMPLOYEE WAGE 
-isPartTime=1;
 isFullTime=2;
+isPartTime=1;
+totalsalary=0;
 empRatePerHour=20;
-randomCheck=$((RANDOM%3));
-
-case $randomCheck in
-	$isFullTime)
-				empHrs=8
-				;;
-	$isPartTime)
-				empHrs=4
-				;;
-	*)
-		empHrs=0
-		;;
-esac
-echo salary=$(( $empHrs*$empRatePerHour ));
+numberWorkingDay=20;
+for(( day=1; day<=$numberWorkingDay; day++ ))
+do
+	empCheck=$(($RANDOM%3))
+		case $empCheck in
+			$isFullTime)
+					empHour=8
+					;;
+			$isPartTime)
+					empHour=4
+					;;
+			*)
+					empHour=0
+					;;
+		esac
+		salary=$(( $empHour * $empRatePerHour ));
+		totalsalary=$(($totalsalary+$salary))
+done
+echo "$salary"
+echo "$totalsalary"
